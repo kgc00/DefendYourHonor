b9825fe163dd71e75339f1fc4e4e2eaa61d396f1
kgc00Zenbook <kirbygc00@gmail.com>
2017-09-02 20:14:20 -0400

    Made Intro + some changes to main scene

---
+++ Assets/Scripts/GameLogicScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogicScript : MonoBehaviour {
+
+	public int insultsReceived = 0;
+	public Slider slider;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			FillBar();
+		}
+	}
+
+	public void FillBar(){
+		if (slider.value <= 5) {
+			insultsReceived++;
+			slider.value = slider.value + 1;
+			Debug.Log (slider.value);
+		} else {
+			Debug.Log ("Can't Increase Value" + slider.value);
+		}
+	}
+}
---
+++ Assets/Scripts/IntroTextScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroTextScript : MonoBehaviour {
+
+	public Text text;
+	public string[] introStory1;
+	public string[] introStory2;
+	string introText1 = "You live a peaceful life Amarillo, until one day...  \nDuring your daily routine: \n\n ";
+	string introText2 = "you stumble upon a rude gentleman. \n";
+	string introText3 = "\n\nThis is the story of that incident.\n(Defend yourself from this foul gentleman\'s profanity!)\n";
+	string completeIntro;
+	int randomStory;
+
+	// Use this for initialization
+	void Start () {
+		randomStory = Random.Range (0, introStory1.Length);
+		introStory1 = new string[3];
+		introStory1[0] = "Inside of the general store, which you\'ve visited to purchase a bottle of milk and a sack of flour ";
+		introStory1[1] = "During an evening gala, which has been most successful, ";
+		introStory1[2] = "In the Sheriff\'s office, after inquiring about the local littering policy, ";
+
+		introStory2 = new string[3];
+		introStory2[0] = "\nOn hearing you\'ve purchased the last bottle of milk he becomes quite belligerent!  ";
+		introStory2[1] = "\nOn hearing your opinion on the benefits of free range cattle raising techniques over similar, but less effective cattle raising methods, he becomes quite incensed.  ";
+		introStory2[2] = "\nOn hearing your inquiry concerning littering he becomes irritated!  'Littering does not concern you!,' he screams.  \nQuite interesting.  ";
+
+		completeIntro = introText1 + introStory1[randomStory] + introText2 + introStory2[randomStory] + introText3;
+
+		text.text = completeIntro;
+	}
+}
