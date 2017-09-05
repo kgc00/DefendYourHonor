f78f7bbee36514e6bcf38d2f46cb852052f34f37
kgc00Zenbook <kirbygc00@gmail.com>
2017-09-04 23:18:42 -0400

    Built Core Features
    Added in insults/definitions
    typing to responde
    meter logic
    game logic
    transition logic

--- Assets/Scripts/GameLogicScript.cs
+++ Assets/Scripts/GameLogicScript.cs
@@ -7,21 +7,162 @@ public class GameLogicScript : MonoBehaviour {
 
 	public int insultsReceived = 0;
 	public Slider slider;
-	
+	public GameObject insultGameObject;
+	public GameObject insultTextTarget;
+	public GameObject responsePanel;
+	public GameObject dictionaryObject;
+	public GameObject finisherPanelObject;
+	public GameObject tutorialPanel;
+	public Font useThisFont;
+	int timesInstantiated = 0;
+	int randomPositionX = 0;
+	int randomPositionY = 0;
+	string[] insultArray;
+	string[] definitionArray;
+	int insultUsed;
+	bool startingInsult = true;
+	bool finisherPanel = false;
+
+
+	void Start(){
+		if(insultGameObject.activeInHierarchy==true){
+			insultGameObject.SetActive (false);
+		}
+		definitionArray = new string[10];
+		definitionArray [0] = "noun: nincompoop\n\na foolish or stupid person.!";
+		definitionArray [1] = "noun: philanderer\n\na man who readily or frequently enters into casual sexual relationships with women; a womanizer.";
+		definitionArray [2] = "adjective: gaseous\n\nrelating to or having the characteristics of a gas. \n\nnoun: fiend\n\nan evil spirit or demon.";
+		definitionArray [3] = "noun: dolt\n\na stupid person.";
+		definitionArray [4] = "adjective, derogatory: puritanical \n\npracticing or affecting strict religious or moral behavior.";
+		definitionArray [5] = "adjective: vile\n\nextremely unpleasant.\n\nnoun: creature\n\nan animal, as distinct from a human being.";
+		definitionArray [6] = "noun: boor\n\nan unrefined, ill-mannered person.";
+		definitionArray [7] = "noun: fool\n\na person who acts unwisely or imprudently; a silly person.";
+		definitionArray [8] = "noun: snake oil salesman\n\nsomeone who knowingly sells fraudulent goods or who is a fraud, quack, or charlatan.";
+		definitionArray [9] = "noun: sophist\n\na person who reasons with clever but fallacious arguments.";
+		insultArray = new string[10];
+		insultArray [0] = "Nincompoop!";
+		insultArray [1] = "Philanderer!";
+		insultArray [2] = "Gaseous Fiend!";
+		insultArray [3] = "Dolt!";
+		insultArray [4] = "Puritanical!";
+		insultArray [5] = "Vile Creature!";
+		insultArray [6] = "Boor!";
+		insultArray [7] = "Fool!";
+		insultArray [8] = "Snake Oil Salesman!";
+		insultArray [9] = "Sophist!";
+		InitiateInsult();
+	}
+
+	public void InitiateInsult()
+	{
+		insultUsed = Random.Range (0, insultArray.Length);
+		randomPositionX = Random.Range (-50, 50);
+		randomPositionY = Random.Range (-75, 75);
+		timesInstantiated += 1;
+		GameObject instantiatedInsultObject = Instantiate(insultGameObject, 
+			new Vector3(insultTextTarget.transform.position.x + (randomPositionX), insultTextTarget.transform.position.y + (randomPositionY), 
+				insultTextTarget.transform.position.z + (10 * timesInstantiated)),
+			Quaternion.identity, insultTextTarget.transform);
+		instantiatedInsultObject.GetComponentInChildren<Text> ().text = insultArray[insultUsed];
+		instantiatedInsultObject.GetComponentInChildren<Text> ().font = useThisFont;
+		instantiatedInsultObject.GetComponentInChildren<Text> ().fontSize = 38;
+		//insultArray [insultUsed].Remove (insultUsed);
+		if (instantiatedInsultObject.activeInHierarchy == false) {
+			instantiatedInsultObject.SetActive (true);
+		}
+		dictionaryObject.GetComponent<Text> ().text = definitionArray [insultUsed];
+		dictionaryObject.GetComponent<Text> ().font = useThisFont;
+		dictionaryObject.GetComponent<Text> ().fontSize = 24;
+		FillBar();
+	}
+
+	public static void Remove<T>(ref T[] arr, int index)
+	{
+		Debug.Log ("Some Logics ");	
+	}
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("space")) {
-			FillBar();
+		if (!finisherPanel) {
+			responsePanel.GetComponent<Text> ().text += Input.inputString;
+			if (Input.GetKeyDown (KeyCode.Backspace)) {
+				responsePanel.GetComponent<Text> ().text = null;
+			}
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				ResolveInsult (responsePanel.GetComponent<Text> ().text);
+				responsePanel.GetComponent<Text> ().text = null;
+			}
+		} else {
+			finisherPanelObject.GetComponent<Text> ().text += Input.inputString;
+			if (Input.GetKeyDown (KeyCode.Backspace)) {
+				finisherPanelObject.GetComponent<Text> ().text = null;
+			}
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				ResolveInsult (finisherPanelObject.GetComponent<Text> ().text);
+				finisherPanelObject.GetComponent<Text> ().text = null;
+			}
 		}
 	}
 
 	public void FillBar(){
-		if (slider.value <= 5) {
-			insultsReceived++;
-			slider.value = slider.value + 1;
-			Debug.Log (slider.value);
+		if(!startingInsult){
+			if (slider.value <= 5) {
+				insultsReceived++;
+				slider.value = slider.value + 1;
+				if (slider.value == 5) {
+					BarMaxed ();
+				}
+			} else {
+				//Debug.Log ("Can't Increase Value" + slider.value);
+				return;
+			}
+		} 
+		startingInsult = false;
+	}
+
+	void ResolveInsult(string responseToInsult){
+		int insultLength = responseToInsult.Length;
+		print (insultLength);
+		if (!finisherPanel) {
+			if (responseToInsult.Length >= 3 && responseToInsult.Contains ("a") || responseToInsult.Contains ("o")
+				|| responseToInsult.Contains ("e") || responseToInsult.Contains ("u") || responseToInsult.Contains ("i") 
+				|| responseToInsult.Contains ("A") || responseToInsult.Contains ("O")
+				|| responseToInsult.Contains ("E") || responseToInsult.Contains ("U") || responseToInsult.Contains ("I")) {
+				//Debug.Log (responseToInsult);	
+				if(slider.value != 5){
+				InitiateInsult ();
+				}
+			}
 		} else {
-			Debug.Log ("Can't Increase Value" + slider.value);
+			if (responseToInsult.Length >= 3 && responseToInsult.Contains ("a") || responseToInsult.Contains ("o")
+				|| responseToInsult.Contains ("e") || responseToInsult.Contains ("u") || responseToInsult.Contains ("i") 
+				|| responseToInsult.Contains ("A") || responseToInsult.Contains ("O")
+				|| responseToInsult.Contains ("E") || responseToInsult.Contains ("U") || responseToInsult.Contains ("I")) {
+				//Debug.Log (responseToInsult);
+				Win ();
+			}
+		}
+
+	}
+
+	void BarMaxed(){
+		//do logic
+		//Debug.Log("BarMaxed");
+		finisherPanel = true;
+		if (!finisherPanelObject.activeInHierarchy){
+			finisherPanelObject.gameObject.transform.parent.gameObject.SetActive (true);
+			responsePanel.transform.parent.gameObject.SetActive (false);
+			dictionaryObject.GetComponent<Text> ().text = "!!!!";
+		}
+	}
+
+	void Win(){
+		GetComponent<LevelManagerScript> ().LoadLevel ("EndScreen");
+	}
+
+	void TutorialLogic(){
+		if (!tutorialPanel.activeInHierarchy) {
+			tutorialPanel.SetActive (true);
 		}
 	}
 }
---
+++ Assets/Scripts/LoadingScreenScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingScreenScript : MonoBehaviour {
+
+	public int level = 0;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Return)){
+			if(level == 1){
+				FindObjectOfType<LevelManagerScript> ().LoadLevel ("IntroScene");
+			} else if (level == 2){
+				FindObjectOfType<LevelManagerScript> ().LoadLevel ("MainScene");
+			} else if (level == 3){
+				FindObjectOfType<LevelManagerScript> ().LoadLevel ("IntroScene");
+			}
+		}
+	}
+}
